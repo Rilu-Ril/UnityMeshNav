eeb0bd3729d1fa3783b6fb35ce382347ba0d8118
Sanria Madzhikova <sanira.ril@gmail.com>
2016-11-16 11:01:24 +0600

    Initial commit

---
+++ Assets/HeroController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HeroController : MonoBehaviour {
+
+	private NavMeshAgent agent;
+
+	// Use this for initialization
+	void Start () {
+		agent = GetComponent<NavMeshAgent> ();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (0)) {
+			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+			RaycastHit result;
+			if (Physics.Raycast (ray, out result, 1000)) {	
+				agent.SetDestination (result.point);
+			}
+		}
+	}
+}
