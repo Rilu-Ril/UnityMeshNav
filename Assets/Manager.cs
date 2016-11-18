ccaf5d5b7ca16b07630e66c2990740470daaee56
Sanria Madzhikova <sanira.ril@gmail.com>
2016-11-18 12:06:56 +0600

    Added canvas and buttons

---
+++ Assets/Manager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Manager : MonoBehaviour {
+
+	public static Manager instance;
+	public bool isPaused = true;
+
+	void Awake() {
+		instance = this;
+		if (instance == null) {
+			instance = this;
+		} else if (instance != this) {
+			Destroy (gameObject);
+		}
+		DontDestroyOnLoad(gameObject);
+
+	}
+	public void exit() {
+		Application.Quit ();
+	}
+
+}
